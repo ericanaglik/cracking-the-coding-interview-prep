9d468cf22d207fd32c8d3a982be5457c3cabd1e5
erica2605@live.com <erica2605@live.com>
2019-12-21 16:46:48 -0800

    updated solution to second problem

--- arrays-and-strings/arrays-and-strings.js
+++ arrays-and-strings/arrays-and-strings.js
@@ -32,15 +32,50 @@ console.log(isUnique('kevin')) // -> returns true
 to decide if one is a permutation of the other. 
 ***********************************************************/
 
+/***** PREVIOUS SOLUTION, changed because of edge case *****
 function checkPermutation(s1,s2) {
     // Base case
     if(s1.length != s2.length) {
         return false
     }
 
+    let characters = new Set()
+    // iterate through the first string, adding each character
+    to the set 
+    for(var i = 0; i < s1.length; i++) {
+        // grab the current character
+        var c = s1.charAt(i)
+        // add it to the set
+        characters.add(c)
+    }
+    // iterate through the second string
+    for(var i = 0; i < s2.length; i++) {
+        // grab the current character
+        var c2 = s2.charAt(i)
+        // if the character isn't in the first set, its not
+        a permutation, so return false
+        if(characters.has(c2) === false) {
+            return false
+        }
+    }
+    // if nothing failed, its a permutation
+    return true
+} */
+
+/************** NEW SOLUTION, edge case passes ************/
+
+function checkPermutation(s1,s2) {
+    // Base case
+    if(s1.length != s2.length) {
+        return false
+    }
+    // returns true if its a permutation, false if not
     return deepEqual(stringToDict(s1), stringToDict(s2))
 }
 
+/* Helper function that compares the keys and values of two 
+objects in javascript. Credit to:
+https://stackoverflow.com/questions/33232823/how-to-compare-two-objects-and-get-key-value-pairs-of-their-differences */
 function deepEqual(x, y) {
     const ok = Object.keys, tx = typeof x, ty = typeof y;
     return x && y && tx === 'object' && tx === ty ? (
@@ -51,14 +86,16 @@ function deepEqual(x, y) {
 
 function stringToDict(s) {
     let dict = new Object()
-    
+    // iterates through the string
     for(var i = 0; i < s.length; i++) {
-        
+        // grabs the current character
         var c = s.charAt(i)
-        
+        /* if its not in the dictionary, add it and set its 
+        value to 0 */
         if(c in dict === false){
             dict[c] = 0
-        } 
+        }
+        // add another occurrence to the key's value
         dict[c]++
     }
     return dict
