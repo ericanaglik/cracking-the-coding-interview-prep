730d7d419683c165224498e09e0fbec2e89e72ec
erica2605@live.com <erica2605@live.com>
2019-12-22 23:19:34 -0800

    added more questions files

---
+++ math-and-logic-puzzles/math-and-logic-puzzles.js
@@ -0,0 +1,148 @@
+/***********************************************************
+6.1: The Heavy Pill - You have 20 bottles of pills. 19
+bottles have 1.0 gram pills, but one has pills of weight 1.1
+grams. Given a scale that provides an exact measurement,
+how would you find the heavy bottle? You can only use the
+scale once.
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.2: Basketball - You have a basketball hoop and someone
+says that you can play one of two games.
+Game 1: You get one shot to make the hoop.
+Game 2: You get three shots and you have to make two of
+three shots.
+If p is the probability of making a particular shot, for
+which values of p should you pick one game or the other?
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.3: Dominoes - There is an 8x8 chessboard in which two
+diagonally opposite corners have been cut off. You are given
+31 dominoes, and a single domino can cover exactly two
+squares. Can you use the 31 dominoes to cover the entire
+board? Prove your answer (by providing an example or showing
+why its impossible).
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.4: Ants on a Triangle - There are three ants on different
+vertices of a triangle. What is the probability of
+collision (between any two or all of them) if they start
+walking on the sides of the triangle? Assume that each ant
+randomly picks a direction, with either direction being 
+equally likely to be chosen, and that they walk at the same
+speed.
+Similarly, find the probability of collision with n ants on
+an n-vertex polygon.
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.5: Jugs of Water - You have a five-quart jug, a
+three-quart jug, and an unlimited supply of water (but no
+measuring cups). How would you come up with exactly four
+quarts of water? Note that the jugs are oddly shaped, such
+that filling up exactly "half" of the jug would be
+impossible.
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.6: Blue-Eyed Island - A bunch of people are living on an
+island, when a visitor comes with a strange order: all blue-
+eyed people must leave the island as soon as possible. There
+will be a flight out at 8:00 pm every evening. Each person
+can see everyone elses eye color, but they do not know
+their own (nor is anyone allowed to tell them).
+Additionally, they do not know how many people have blue
+eyes, although they do know that at least one person does. 
+How many days will it take the blue-eyed people to leave?
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.7: The Apocalypse - In the new post-apocalyptic world, the
+world queen is desperately concerned about the birth rate.
+Therefore, she decrees that all families should ensure that
+they have one girl or else they face massive fines. If all
+families abide by this policy -- that is, they have to
+continue to have children until they have one girl, at which
+point they immediately stop -- what will the gender ratio of
+the new generation be? (Assume that the odds of someone
+having a boy or a girl on any given pregnancy is equal.)
+Solve this out logically and then write a computer
+simulation of it.
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.8: The Egg Drop Problem - There is a building of 100
+floors. If an egg drops from the Nth floor or above, it will
+break. If its dropped from any floor below, it will not
+break. You're given two eggs. Find N, while minimizing the
+number of drops for the worst case.
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.9: 100 Lockers - There are 100 closed lockers in a
+hallway. A man begins by opening all 100 lockers. Next, he
+closes every second locker. Then, on his third pass, he
+toggles every third locker (closes it if it is open or opens
+it if it is closed). This process continues for 100 passes,
+such that on each pass i, the man toggles every ith locker.
+After his 100th pass in the hallway, in which he toggles
+only locker #100, how many lockers are open? 
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
+
+/***********************************************************
+6.10: Poison - You have 1000 bottles of soda, and exactly
+one is poisoned. You have 10 test strips which can be used
+to detect poison. A single drop of poison will turn the
+test strip positive permanently. You can put any number of
+drops ona  test strip at once and you can reuse a test strip
+as many times as you'd like (as long as the results are
+negative). However, you can only run tests once per day and
+it takes seven days to return a result. How would you figure
+out the potential poisoned bottle in as few days as
+possible?
+
+FOLLOW UP
+Write code to simulate your approach.
+***********************************************************/
+
+/********************** MY SOLUTION ************************
+
+***********************************************************/
---
+++ trees-and-graphs/trees-and-graphs.js
@@ -0,0 +1,153 @@
+/***********************************************************
+4.1: Route Between Nodes - Given a directed graph, design an
+algorithm to find out whether there is a route between two
+nodes
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.2: Minimal tree - Given a sorted (increasing order) array
+with unique integer elements, write an algorithm to create a
+binary search tree with minimal height.
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.3: List of Depths - Given a binary tree, design an
+algorithm which creates a linked list of all the nodes at
+each depth (e.g., if you have a tree with depth D, you'll
+have D linked lists).
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.4: Check Balanced - Implement a function to check if a
+binary tree is balanced. For the purposes of this question,
+a balanced tree is defined to be a tree such that the
+heights of the two subtrees of any node never differ by more
+than one.
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.5: Validate BST - Implement a function to check if a
+binary tree is a binary search tree.
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.6: Successor - Write an algorithm to find the "next" node
+(i.e., in-order successor) of a given node in a binary
+search tree. You may assume that each node has a link to its
+parent.
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.7: Build Order - You are given a list of projects and a
+list of dependencies (which is a list of pairs of projects,
+where the second project is dependent on the first project).
+All of a project's dependencies must be built before the
+project is. Find a build order that will allow the projects
+to be built. If there is no valid build order, return an
+error.
+
+EXAMPLE
+Input:
+  projects: a, b, c, d, e, f
+  dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
+Output: f, e, a, b, d, c
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.8: First Common Ancestor - Design an algorithm and write
+code to find the first common ancestor of two nodes in a
+binary tree. Avoid storing additional nodes in a data
+structure. NOTE: This is not necessarily a binary search
+tree. 
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.9: BST Sequences - A binary search tree was created by
+traversing through an array from left to right and inserting
+each element. Given a binary search tree with distinct
+elements, print all possible arrays that could have led to
+this tree.
+
+EXAMPLE:
+Input: 
+            (2)
+          /     \
+        (1)     (3)
+Output: {2, 1, 3},{2, 3, 1}
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.10: Check Subtree - T1 and T2 are two very large binary
+trees, with T1 much bigger than T2. Create an algorithm to
+determine if T2 is a subtree of T1.
+A tree T2 is a subtree of T1 if there exists a node n in T1
+such that the subtree of n is identical to T2. That is, if
+you cut off the tree at node n, the two trees would be
+identical.
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.11: Random Node - You are implementing a binary tree class
+from scratch which, in addition to insert, find, and delete,
+has a method getRandomNode() which returns a random node
+from the tree. All nodes should be equally likely to be
+chosen. Design and implement an algorithm for getRandomNode,
+and explain how you would implement the rest of the methods. 
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
+
+/***********************************************************
+4.12: Paths with Sum - You are given a binary tree in which
+each node contains an integer value (which might be positive
+or negative). Design an algorithm to count the number of
+paths that sum to a given value. The path does not need to
+start or end at the root or a leaf, but it must go downwards
+(traveling only from parent nodes to child nodes).
+***********************************************************/
+
+/******************** BIG O NOTATION ***********************
+
+***********************************************************/
