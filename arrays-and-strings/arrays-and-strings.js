84cc34dea64416f101b5ba92f6378e30c66df063
erica2605@live.com <erica2605@live.com>
2019-12-22 18:15:34 -0800

    wrote base case and tests for one away

--- arrays-and-strings/arrays-and-strings.js
+++ arrays-and-strings/arrays-and-strings.js
@@ -219,6 +219,25 @@ pale, bale -> true
 pale, bake -> false
 ***********************************************************/
 
+function oneAway(s1,s2){
+    // Base Case
+    /* If the length of the strings are more than one apart,
+    they are automatically not one away, therefore return
+    false */
+    if(Math.abs(s1.length - s2.length) > 1) {
+        return false
+    }
+    return true
+}
+
+// Tests 
+console.log("Problem 1.5 One Away")
+console.log(oneAway("pales","pale")) // -> returns true
+console.log(oneAway("kevin","dog")) // -> returns false
+console.log(oneAway("pale","bale")) // -> returns true
+console.log(oneAway("pale","bake")) // -> returns false
+
+
 /***********************************************************
 1.6: String Compression - Implement a method to perform
 basic string compression using the counts of repeated 
