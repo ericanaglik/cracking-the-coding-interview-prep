563b46fda458ad11dc6a67713b6e704ea7723f4b
erica2605@live.com <erica2605@live.com>
2019-12-22 22:06:28 -0800

    kth to last complete

--- linked-lists/linked-lists.js
+++ linked-lists/linked-lists.js
@@ -11,14 +11,14 @@ buffer is not allowed?
 
 ***********************************************************/
 
-function removeDups(ll){
+// function removeDups(ll){
 
-}
+// }
 
 // Tests
-let linkedList1 = new LinkedList(1,2,1,1,3)
-console.log("Problem 2.1 Remove Dups")
-console.log(removeDups(linkedList1))
+// let linkedList1 = new LinkedList(1,2,1,1,3)
+// console.log("Problem 2.1 Remove Dups")
+// console.log(removeDups(linkedList1))
 
 
 /***********************************************************
@@ -27,8 +27,29 @@ kth to last element of a singly linked list.
 ***********************************************************/
 
 /******************** BIG O NOTATION ***********************
-
+kthToLast() is O(n - k) where n is the length of the list
+and k is the desired position
 ***********************************************************/
+
+function kthToLast(ll, k){
+  /* We want to access the kth position from the last node,
+  so we subtract the length by k to get the number of
+  iterations we need to do through the linked list */
+  let position = ll.length - k
+  // set the current node to be the head node
+  let currNode = ll.head
+  // move the current node until you get to the position
+  for(let i = 1; i < position; i++){
+    currNode = currNode.next
+  }
+  // return the Kth to last's node's value
+  return currNode.value
+}
+
+// Tests 
+let testKthToLast = new LinkedList(1,2,1,1,3,4,5,6)
+console.log("Problem 2.2 Return Kth to Last")
+console.log(kthToLast(testKthToLast,1)) // -> returns 5
 
 /***********************************************************
 2.3: Delete Middle Node - Implement an algorithm to delete
