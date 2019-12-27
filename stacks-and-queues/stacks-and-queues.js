7ac0238ed66cd6f2e6c9d62624133406a4b088d9
erica2605@live.com <erica2605@live.com>
2019-12-27 17:15:45 -0500

    3.1 complete

--- stacks-and-queues/stacks-and-queues.js
+++ stacks-and-queues/stacks-and-queues.js
@@ -4,8 +4,15 @@ const Deque = require('./deque')
 array to implement three stacks.
 ***********************************************************/
 
-/******************** BIG O NOTATION ***********************
-
+/************************ ANSWER ***************************
+I would have three markers that mark where each stack ends.
+If I wanted to add something to a stack I would insert the
+item before the ending marker for that stack. Then I would
+increment all the markers to the right of the item I 
+inserted. If I wanted to remove something from the stack I
+would find the marker that ends the stack and then pop the 
+item from the left of the marker and then decrement every
+marker to the right.
 ***********************************************************/
 
 /***********************************************************
