53868371bc5f9a431f0e96cfa5189a17c5c060b8
erica2605@live.com <erica2605@live.com>
2020-05-13 14:47:46 -0700

    added questions and answers to assignment 4

---
+++ practice-problems/assignemnt-4.py
@@ -0,0 +1,22 @@
+# Rotate a given linked list counter-clockwise by k nodes, where k is a given integer.
+
+# What clarifying questions would you ask?
+# Is it a singly linked list or doubly linked?
+# Is there a tail pointer or just a head pointer
+
+# What reasonable assumptions could you state?
+# I am assuming its a singly linked list and that there is a head pointer
+
+# What are 2-3 ways you can simplify the problem?
+# Instead of rotating k nodes I could rotate once to simplify
+
+# Brainstorm 2-3 ways to approach the problem.
+# to rotate counter clockwise, get rid of the connection
+# between the head and the second node, make the head the new
+# tail and make the second node the new head
+
+# Choose one idea and write pseudocode for it.
+
+
+
+
