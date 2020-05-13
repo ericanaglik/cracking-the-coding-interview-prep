976f3926dbd28b0d6111b6cc10414f54e6741ee2
erica2605@live.com <erica2605@live.com>
2020-05-13 15:00:27 -0700

    assignment 4 complete

--- practice-problems/assignment-4.py
+++ practice-problems/assignment-4.py
@@ -5,7 +5,7 @@
 # Is there a tail pointer or just a head pointer
 
 # What reasonable assumptions could you state?
-# I am assuming its a singly linked list and that there is a head pointer
+# I am assuming its a singly linked list and that there is a head pointer and a tail pointer
 
 # What are 2-3 ways you can simplify the problem?
 # Instead of rotating k nodes I could rotate once to simplify
@@ -17,6 +17,18 @@
 
 # Choose one idea and write pseudocode for it.
 
-
+def rotate_ll(ll, k):
+    # DO THIS K TIMES
+    for i in range(k):
+        # make a variable that points to the head node called current
+        current = ll.head
+        # set head to current.next
+        ll.head = current.next
+        # set currents next pointer to none
+        current.next = None
+        # make tail point to current instead of none
+        ll.tail.next = current
+        
+    return ll
 
 
